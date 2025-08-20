6a6bf6734d302d6488e5ce55c0f33bf8e1d8e45a
Tia Piol <tiarox02@yahoo.com>
2025-08-20 10:04:24 -0700

    build the hoot list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router';
 
 import NavBar from './components/NavBar/NavBar';
@@ -6,19 +6,39 @@ import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
+import HootList from './components/HootList/HootList';
+import * as hootService from "./services/hootService";
 
 import { UserContext } from './contexts/UserContext';
 
 const App = () => {
   const { user } = useContext(UserContext);
-  
+  const [hoots, setHoots] = useState([]);
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+      setHoots(hootsData);
+    };
+    if (user) fetchAllHoots();
+  }, [user]);
+
+
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <Routes>
         <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
+        {user ? (
+          <>
+            <Route path='/hoots' element={<HootList hoots={hoots}/>} />
+          </>
+        ) : (
+          <>
+            <Route path='/sign-up' element={<SignUpForm />} />
+            <Route path='/sign-in' element={<SignInForm />} />
+          </>
+        )}
       </Routes>
     </>
   );
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,7 +16,8 @@ const NavBar = () => {
       {user ? (
         <ul>
           <li>Welcome, {user.username}</li>
-          <li><Link to='/'>Dashboard</Link></li>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/hoots'>HOOTS</Link></li>
           <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
         </ul>
       ) : (
